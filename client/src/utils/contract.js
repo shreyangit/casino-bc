7b93701a1e4d94223ac9c7635dc891af1082555e
shreyangit <shreyan.s.narula@gmail.com>
2025-11-01 17:02:04 +0530

    hello

--- client/src/utils/contract.js
+++ client/src/utils/contract.js
@@ -2,7 +2,7 @@ import { ethers } from "ethers";
 import contractABI from "../abi/ContractABI.json"; // put your ABI JSON path here
 
 // Replace this with the deployed address from Anmol
-const contractAddress = "0x7EF2e0048f5bAeDe046f6BF797943daF4ED8CB47";
+const contractAddress = "0x1ff2b86a5dB40632DAB47f8A775f91Bc5b40028A";
 
 export const getEthereumContract = async () => {
   if (!window.ethereum) {
